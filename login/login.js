73f115824a127620ab10fb8858e15266b7739e39
ahmedmahmoudac <76945102+ahmedmahmoudac@users.noreply.github.com>
2023-07-18 20:04:12 +0300

    Update login.js

--- login/login.js
+++ login/login.js
@@ -99,56 +99,44 @@ document.querySelector(".btn-sign-in").addEventListener("click",async()=>{
 
 
 /* start create account */
+
+
 document.querySelector(".btn-sign-up").addEventListener("click", () => {
+  let username = document.querySelector(".username-up").value;
+  let password = document.querySelector(".password-up").value;
+  let password2 = document.querySelector(".password-up-2").value;
+  let email = document.querySelector(".email-up").value;
+
+  if (username !== "" && password !== "" && password2 !== "" && email !== "" && password === password2) {
+    let id = Math.floor(Math.random() * 100000000);
+    setDoc(doc(db, "accounts", `${id}`), {
+      id: id,
+      username: username,
+      password: password,
+      email: email,
+      time: Date.now(),
+      N: "",
+    });
+
+    document.querySelector(".username-up").value = "";
+    document.querySelector(".password-up").value = "";
+    document.querySelector(".email-up").value = "";
+    document.querySelector(".password-up-2").value = "";
+
     Swal.fire(
-        'Account Creation Disabled 😥',
-        'Sorry, account creation is currently disabled for New Users, if you already has an account you can sign in with it. 😥',
-        'warning'
+      'تم انشاء الحساب',
+      'يمكنك الان تسجيل الدخول',
+      'success'
     );
+
+    document.querySelector("#tab-1").click();
+  } else if (username !== "" && password !== password2 && email !== "") {
+    Swal.fire("", "The Two Password Should be the Same", "");
+  } else {
+    Swal.fire("", "Enter Username, Password, and Email", "");
+  }
 });
 
-// document.querySelector(".btn-sign-up").addEventListener("click",()=>{
- //   let username = document.querySelector(".username-up").value
- //   let password = document.querySelector(".password-up").value
- //   let password2 = document.querySelector(".password-up-2").value
- //   let email = document.querySelector(".email-up").value
-
-
- //   if(username!=""&&password!=""&&password2!=""&&email!=""&&password==password2)
- //   {
-
-    //  let id = Math.floor(Math.random() * 100000000);
-    //  setDoc(doc(db,"accounts",`${id}`),{
-    //    id: id,
-    //    username: username,
-    //    password: password,
-    //    email: email,
-    //    time: Date.now(),
-   //     N:"",
-  //    });
-  
-
-   //   document.querySelector(".username-up").value=""
-    //  document.querySelector(".password-up").value=""
-    //  document.querySelector(".email-up").value=""
-    //  document.querySelector(".password-up-2").value=""
-
-      /**/
-    //  Swal.fire(
-    //    'تم انشاء الحساب',
-    //    'يمكنك الان تسجيل الدخول',
-     //   'success'
-  //    )
-      /**/
-
-  //    document.querySelector("#tab-1").click()
-
-  //  } else if(username!=""&&password!=password2&&email!="") {
-  //      Swal.fire("","The Two Password Should be the Same","")
-  //  } else {
-  //      Swal.fire("","Enter Username,Password and Email","")
- //   }
-// })
 
 /* end create account */
 
